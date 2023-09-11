2c584fa40cd713550152244d80a7672b9d355f22
Gowtham261 <72153201+Gowtham261@users.noreply.github.com>
2023-09-11 11:34:29 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace postive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num > 0)
+            {
+                Console.WriteLine("positive");
+                return;
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine("negative");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Zero");
+                return;
+            }
+        }
+    }
+}
