5e9c76bc4257139f6b6a9fbba1d4732fcef38109
Gowtham261 <72153201+Gowtham261@users.noreply.github.com>
2023-09-11 11:01:11 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,18 +4,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assignement_3
+namespace code_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("num1 + num2");
-            Console.WriteLine("num1 - num2");
-            Console.WriteLine("num1 % num2");
-            Console.WriteLine("num1 * num2");
+            Console.WriteLine("how many per week");
+            int[] Cigi = new int[10];
+            for (int i = 0; i < Cigi.Length; i++) 
+            {
+                Cigi[i] = Convert.ToInt32(Console.ReadLine());
+            }
+           
+            int sum = Cigi.Sum();
+            Console.WriteLine("sum " + sum);
+           
+            double avg = Cigi.Average();
+            Console.WriteLine("average " + avg);
+            Console.WriteLine("maxmium");
+            int max = Cigi.Max();
+            Console.WriteLine("minium");
+            int mini = Cigi.Min();
+            Console.WriteLine("assending");
+            Array.Sort(Cigi);
+            for (int i = 0; i < Cigi.Length; i++) 
+            {
+                Console.WriteLine(Cigi[i]);
+            }
+            Console.WriteLine("dessending");
+            Array.Reverse(Cigi);
+            for(int i = 0; i<Cigi.Length; i++)
+            {
+                Console.WriteLine(Cigi[i]);
+            }
+            Console.Read();
+
 
         }
     }
