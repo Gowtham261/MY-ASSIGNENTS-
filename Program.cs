7576cd29efecfaa7d19708a0c1a4069abbe85203
Gowtham261 <72153201+Gowtham261@users.noreply.github.com>
2023-09-11 11:20:25 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignement_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("num1 + num2");
+            Console.WriteLine("num1 - num2");
+            Console.WriteLine("num1 % num2");
+            Console.WriteLine("num1 * num2");
+
+        }
+    }
+}
