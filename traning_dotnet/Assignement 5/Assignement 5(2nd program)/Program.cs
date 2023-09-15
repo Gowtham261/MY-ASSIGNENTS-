2999cdcc86843b0a583e32f2198e4c0701eb673f
Gowtham261 <72153201+Gowtham261@users.noreply.github.com>
2023-09-15 15:22:09 +0530

    Add files via upload

---
+++ traning_dotnet/Assignement 5/Assignement 5(2nd program)/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignement_5_2nd_program_
+{
+    class Customer
+    {
+        static void Main(string[] args)
+        {
+            int customerid;
+            customerid = Convert.ToInt32(Console.Read());
+            string name;
+            Console.WriteLine("enter name");
+            string city;
+            Console.WriteLine("enter city");
+            int phonumber;
+            phonumber = Convert.ToInt32(Console.Read());
+            int age;
+            age = Convert.ToInt32(Console.Read());
+
+        }
+        class Customerinfo
+        {
+            public string Name;
+            public string City;
+            public int Phonumber;
+            public int Age;
+            public int Custid;
+
+            public Customerinfo(string name, string city, int age, int custid, int phonumber)
+            {
+                Age = age;
+                City = city;
+                Custid = custid;
+                Name = name;
+                Phonumber = phonumber;
+            }
+            static void DisplayCustomerinfo()
+            {
+                Console.WriteLine("age,city,custid,name,phonumber");
+            }
+            Customerinfo()
+            {
+                Console.WriteLine("Destructor Is Activated");
+            }
+
+        }
+    }
+}
---
+++ traning_dotnet/Assignement 5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignement_5
+{
+    class Program
+    {
+        static string firstname; static string lastname;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter First name");
+            firstname = Console.ReadLine();
+            Console.WriteLine("Enter lastname name");
+            lastname = Console.ReadLine();
+            Console.Read();
+        }
+
+        public static void Display()
+        {
+            Console.WriteLine(firstname.ToUpper());
+            Console.WriteLine(lastname.ToUpper());
+            Console.Read();
+        }
+    }
+}
