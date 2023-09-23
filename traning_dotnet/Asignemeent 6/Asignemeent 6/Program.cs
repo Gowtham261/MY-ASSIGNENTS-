cd0485bee9a833feab588151898ea0bba11a161e
Gowtham261 <72153201+Gowtham261@users.noreply.github.com>
2023-09-23 16:05:51 +0530

    Add files via upload

---
+++ traning_dotnet/Asignemeent 6/Asignemeent 6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using assssignement_6;
+
+namespace Asignemeent_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Booking ki swagatham");
+            Console.WriteLine("enter Customername");
+            string Customername = Console.ReadLine();
+            Console.WriteLine(" enter CustomerAge");
+            int CustomerAge = Convert.ToInt32(Console.Read());
+            Concession siva = new Concession();
+            siva.
+        }
+    }
+
+}
