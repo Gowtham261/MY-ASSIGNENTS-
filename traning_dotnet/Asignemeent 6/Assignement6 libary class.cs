aadb49a4cdee8c1a20688b6aafefd3d080ab8a3b
Gowtham261 <72153201+Gowtham261@users.noreply.github.com>
2023-09-23 16:22:03 +0530

    Add files via upload

---
+++ traning_dotnet/Asignemeent 6/Assignement6 libary class.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using assignement_6
+
+
+namespace assignement_6
+{
+    public class Concession
+
+    {
+
+        int TotalFare = 700;
+
+        public static CalculateConcession(int age, string name)
+
+        {
+
+            if (age <= 5)
+
+            {
+
+                Console.WriteLine("Little Champs- Free Ticket");
+
+            }
+            else if (age > 60)
+
+            {
+
+                Amount = TotalFare * 0.3;
+
+                CutFare = TotalFare - Amount;
+
+                Console.WriteLine("Aged people");
+            }
+        }
+    }
---
+++ traning_dotnet/Asignemeent 6/assignement6 main class.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignemeent_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Welcome to ticket booking");
+
+            Console.Write("Enter your name");
+
+            string name = Console.ReadLine();
+
+            Console.Write("Enter your age");
+
+            int age = Convert.ToInt32(Console.Read());
+
+            if (int.TryParse(Console.ReadLine(), out age))
+
+            {
+
+                string concessionDetails = Concession.ConcessionCalculator.CalculateConcession(age, name);
+
+                Console.WriteLine(concessionDetails);
+
+            }
+            else
+            {
+
+                Console.WriteLine("Incorrcet input.");
+
+            }
+             Console.ReadLine();
+
+        }
+
+    }
+
+
+
+}
