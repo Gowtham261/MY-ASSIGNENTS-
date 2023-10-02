5112e02057a1bc861837130b37dfa766f14c34e8
Gowtham261 <72153201+Gowtham261@users.noreply.github.com>
2023-10-02 21:03:03 +0530

    Add files via upload

---
+++ traning_dotnet/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CaseStudy3and4
+{
+    internal class Student
+    {
+        private int id;
+        private string name;
+        private DateTime now;
+
+        public Student(int id, string name, DateTime now)
+        {
+            this.id = id;
+            this.name = name;
+            this.now = now;
+        }
+    }
+}
