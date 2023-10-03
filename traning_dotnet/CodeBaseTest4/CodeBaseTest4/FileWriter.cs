49eec49eb1c90fd8ce0977bba1bf45406a7c8b3a
Gowtham261 <72153201+Gowtham261@users.noreply.github.com>
2023-10-03 17:50:32 +0530

    Add files via upload

---
+++ traning_dotnet/CodeBaseTest4/CodeBaseTest4/FileWriter.cs
@@ -0,0 +1,14 @@
+﻿namespace CodeBaseTest4
+{
+    internal class FileWriter
+    {
+        private string file;
+        private bool v;
+
+        public FileWriter(string file, bool v)
+        {
+            this.file = file;
+            this.v = v;
+        }
+    }
+}
