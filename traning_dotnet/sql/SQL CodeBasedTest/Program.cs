73bd5b36ecc9c97aacc3c0df6bd0c7c8c76a081e
Gowtham261 <72153201+Gowtham261@users.noreply.github.com>
2023-10-23 11:21:37 +0530

    Add files via upload

---
+++ traning_dotnet/sql/SQL CodeBasedTest/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeBasedTest_6_SQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ traning_dotnet/sql/SQL CodeBasedTest/SQL CodeBasedTest 6.sql
@@ -0,0 +1,16 @@
+create table Code_Employeee
+(
+empno int primary key,
+empname varchar(35) NOT NULL,
+empsal numeric(10,2) CHECK (empsal >= 2500),
+emptype char(1) CHECK (emptype IN ('F','P'))
+);
+CREATE PROCEDURE THEEmployee
+@empname VARCHAR(35),
+@empsal numeric(10,2),
+@emptype char(1)
+as 
+begin 
+insert into Code_Employeee (empname,empsal,emptype)
+values (@empname,@empsal,@emptype)
+END;
